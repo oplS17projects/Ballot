4d8b8d0cfd7c0005677513805df8b3a71279c011
Brandon Karl <brandon@bkarl.com>
2017-04-17 18:58:27 -0400

    Added algorithms to retrieve json data

--- Ballot.rkt
+++ Ballot.rkt
@@ -16,6 +16,13 @@
 (define food "sushi")
 (define location "boston")
 
+;Combines all the info
+(define (combiner lst1 lst2 lst3 lst4)
+  (if (null? lst1)
+      '()
+      (cons (list (car lst1) (car lst2) (car lst3) (car lst4)) (combiner (cdr lst1) (cdr lst2) (cdr lst3) (cdr lst4))))
+  )
+
 ;Creates a link to retrieve the info
 (define url (string-append "http://ballotyelp.herokuapp.com/yelpsearch/" food "/" location))
 (define myurl (string->url url))
@@ -23,23 +30,29 @@
 (define response (port->string myport))
 
 ;Uses port and creates a JSON
-(define json-data (with-input-from-string response (λ () (read-json))))
+(define yelp (with-input-from-string response (λ () (read-json))))
 
-;First layer of the JSON is the businesses
-(define businesses (hash-ref json-data 'businesses))
+;First layer of the JSON is the businesses, uses fold
+(define businessData (foldr cons '() (hash-ref yelp 'businesses)))
 
-;Second layer is the individual business information, first layer returns a list, so
-;we need to use the dirst elemtn of this list
-(define name (hash-ref (car businesses) 'name))
-(define price (hash-ref (car businesses) 'price))
+;Gets all the information from the JSON, uses map
+(define businessNames (map (lambda (x) (hash-ref x 'name)) businessData))
+(define businessPhotos (map (lambda (x) (hash-ref x 'image_url)) businessData))
+(define businessPrice (map (lambda (x) (hash-ref x 'price (lambda () "No price found!"))) businessData))
+(define businessPhone (map (lambda (x) (hash-ref x 'display_phone (lambda () "No number found!"))) businessData))
+
+;Combines all the data into a usable list, where each element is a different resteraunt
+(define combinations (combiner businessNames businessPhotos businessPrice businessPhone))
+
+
 
 ;Net/url lib rary does not support retrieving images and printing them, but 2htdp does.
 ;Basically net-url for images
-(define image (bitmap/url (hash-ref (car businesses) 'image_url)))
+;(define image (bitmap/url (hash-ref (car businesses) 'image_url)))
 
 ;Prints the info for the first location
-(define (print-text-info) (printf "We think you should go to ~a, it costs ~a.~n" name price))
-(define (show-info) (begin (print-text-info) image))
+;(define (print-text-info) (printf "We think you should go to ~a, it costs ~a.~n" name price))
+;(define (show-info) (begin (print-text-info) image))
 
 
 
